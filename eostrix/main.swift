e5e2ece8f162d8e5692a7b2252f6669a5262030b
Sylvan M <alephnulltim@gmail.com>
2019-05-04 21:00:34 -0400

    Initial Commit

---
+++ eostrix/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  eostrix
+//
+//  Created by Sylvan Martin on 5/4/19.
+//  Copyright © 2019 Sylvan Martin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
